a696194af3433aab3940e136141b1f0d7dba9320
Savannah Fox <savannahfox@gmail.com>
2019-01-22 10:48:35 -0800

    Working entity framework changes.

--- NewsletterApp/NewsletterApp/Model1.Designer.cs
+++ NewsletterApp/NewsletterApp/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\My DELL\Documents\GitHub\The-Tech-Academy-C-Sharp-Coding-Projects\NewsletterApp\NewsletterApp\Newsletter.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\My DELL\Documents\GitHub\The-Tech-Academy-C-Sharp-Coding-Projects\NewsletterApp\NewsletterApp\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

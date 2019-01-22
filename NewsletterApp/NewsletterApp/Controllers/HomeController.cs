11d5329ee03db6fabc71cb05c921c9c427975b2a
Savannah Fox <savannahfox@gmail.com>
2019-01-21 16:36:54 -0800

    Add admin page for sign-ups.

--- NewsletterApp/NewsletterApp/Controllers/HomeController.cs
+++ NewsletterApp/NewsletterApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsletterApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,6 +11,11 @@ namespace NewsletterApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = @"Data Source=MYDELL-PC\SQLEXPRESS;Initial Catalog=Newsletter;
+                                            Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                            TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                            MultiSubnetFailover=False";
+
         public ActionResult Index()
         {
             return View();
@@ -24,11 +30,6 @@ namespace NewsletterApp.Controllers
             }
             else
             {
-                string connectionString = @"Data Source=MYDELL-PC\SQLEXPRESS;Initial Catalog=Newsletter;
-                                            Integrated Security=True;Connect Timeout=30;Encrypt=False;
-                                            TrustServerCertificate=False;ApplicationIntent=ReadWrite;
-                                            MultiSubnetFailover=False";
-
                 string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
                                         (@FirstName, @LastName, @EmailAddress)";
 
@@ -51,18 +52,30 @@ namespace NewsletterApp.Controllers
             }
         }
 
-        public ActionResult About()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your application description page.";
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress from SignUps";
+            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
 
-            return View();
-        }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                connection.Open();
 
-            return View();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsletterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signups.Add(signup);
+                }
+            }
+            return View(signups);
         }
     }
 }

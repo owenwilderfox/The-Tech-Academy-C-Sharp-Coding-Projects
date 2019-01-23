c0422ad8ff7abf6c3b3f924b7e21b0dac5676352
Savannah Fox <savannahfox@gmail.com>
2019-01-22 17:23:18 -0800

    Move project to repository.

---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Quote(string FirstName, string LastName, string EmailAddress, string DateOfBirth, string CarYear, string CarMake,
+            string CarModel, string Dui, string MovingViolations, string CoverageType)
+        {
+
+        }
+    }
+}
